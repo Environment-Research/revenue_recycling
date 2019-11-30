62c5e4218d0cc1ccc72429d757d73b05880eccde
Frank <frankerrickson@berkeley.edu>
2019-11-29 17:16:19 -0800

    Add files to run NICE revenue recycling model

---
+++ src/helper_functions.jl
@@ -0,0 +1,119 @@
+# #------------------------------------------------------------------------------------------------------------------
+# #------------------------------------------------------------------------------------------------------------------
+# # This file contains functions used for creating and running the Nested Inequalities Climate Economy (NICE) model.
+# #------------------------------------------------------------------------------------------------------------------
+# #------------------------------------------------------------------------------------------------------------------
+
+
+#####################################################################################################################
+# CALCULATE DAMAGE, CO₂ MITIGATION COST, OR CO₂ TAX BURDEN DISTRIBUTIONS ACROSS AN INDIVIDUAL REGION'S QUINTILES.
+#####################################################################################################################
+# Description: This function will calculate quintile distribution shares for an individual RICE region based
+#              on a provided income elasticity.
+#
+# Function Arguments:
+#
+#       elasticity    = Income elasticity of climate damages, CO₂ mitigation costs, CO₂ tax burdens, etc.
+#       income_shares = A vector of quintile income shares for a given RICE region.
+#--------------------------------------------------------------------------------------------------------------------
+
+function regional_quintile_distribution(elasticity, income_shares)
+
+    # Apply elasticity to quintile income shares.
+    scaled_shares = income_shares .^ elasticity
+
+    # Allocate empty array for distribution across quintiles resulting from the elasticity.
+    updated_quintile_distribution = zeros(5)
+
+    # Loop through each quintile to calculate updated distribution.
+    for q in 1:5
+        updated_quintile_distribution[q] = scaled_shares[q] ./ sum(scaled_shares[:])
+    end
+
+    return updated_quintile_distribution
+end
+
+
+#####################################################################################################################
+# PERFORM A REGRESSION.
+#####################################################################################################################
+# Description: This function will perform a regression of 'Y' against a user-supplied 'Data' term.
+#
+# Function Arguments:
+#
+#       Y    = Dependent variable in regression.
+#       Data = Independent data (x-terms) in regression.
+#--------------------------------------------------------------------------------------------------------------------
+
+function regress(Y, Data)
+
+    # Get size of data.
+    n = length(Data[:,1])
+    k = length(Data[1,:])
+
+    # Add column of ones.
+    X = [ones(n) Data]
+
+    # Calculate beta coefficient.
+    x_prime_inv = inv(X' * X)
+    betahat = x_prime_inv * X' * Y
+
+    # Calculate residuals.
+    u = Y - X * betahat
+
+    # Calculate hetero var-cov matrix.
+    X_eps = zeros(n,k+1)
+    for i = 1:n
+        X_eps[i,:] = u[i] * X[i,:]
+    end
+
+    varhetero = x_prime_inv * X_eps' * X_eps * x_prime_inv
+
+    return betahat, varhetero
+end
+
+
+#####################################################################################################################
+# PERFORM A META-REGRESSION OVER MULTIPLE ELASTICITY STUDIES.
+#####################################################################################################################
+# Description: This function will perform a regression (elasticity vs. ln gdp per capita) across the results of
+#              multiple studies to provide the coefficients for a time-varying income elasticity of CO₂ price
+#              exposure relationship.
+#
+# Function Arguments:
+#
+#       Data = Independent data (x-terms) in regression coming from multiple studies.
+#--------------------------------------------------------------------------------------------------------------------
+
+function meta_regression(Data)
+
+    # Sort study data by region and country names.
+    sort!(Data, [:Region, :Country])
+    n = size(Data, 1)
+
+    # Allocate array to store elastcities.
+    elasticities = zeros(n)
+
+    # Isolate variables needed for each run (quintile fuel spending and income shares).
+    fuel_shares   = [:Q1FS, :Q2FS, :Q3FS, :Q4FS, :Q5FS]
+    income_shares = [:Q1IncS, :Q2IncS, :Q3IncS, :Q4IncS, :Q5IncS]
+
+    # Loop over each study and calculate elasticity with a log-log regression.
+    for i = 1:n
+
+        # Take logs of variables
+        log_income_share = log.(Vector(Data[i, income_shares]))
+        log_fuel_total   = log.(Vector(Data[i, fuel_shares]) .* Vector(Data[i, income_shares]))
+
+        # Run log-log regression and store elasticity.
+        B, V   = regress(log_fuel_total, log_income_share)
+        elasticities[i] = B[2]
+    end
+
+    # Using these elasticities, run meta-regression (elasticity vs. ln gdp per capita).
+    meta_B, meta_V  = regress(elasticities, log.(Data.GDP))
+    meta_intercept  = meta_B[1]
+    meta_slope      = meta_B[2]
+
+    return meta_intercept, meta_slope
+end
---
+++ src/revenue_recycling_components/nice_revenue_recycle_component.jl
@@ -0,0 +1,97 @@
+@defcomp nice_recycle begin
+
+    # --------------------
+    # Model Indices
+    # --------------------
+
+    regions                  = Index()                                     # Index for RICE regions.
+    quintiles                = Index()                                     # Index for regional income quintiles.
+
+    # --------------------
+    # Model Parameters
+    # --------------------
+
+    min_study_gdp            = Parameter()                                 # Minimum observed per capita GDP value found in elasticity studies ($/person).
+    max_study_gdp            = Parameter()                                 # Maximum observed per capita GDP value found in elasticity studies ($/person).
+    elasticity_intercept     = Parameter()                                 # Intercept term to estimate time-varying income elasticity.
+    elasticity_slope         = Parameter()                                 # Slope term to estimate time-varying income elasticity.
+    regional_population      = Parameter(index=[time, regions])            # Regional population levels (millions of people).
+    ABATEFRAC                = Parameter(index=[time, regions])            # Cost of CO₂ emission reductions as share of gross economic output.
+    DAMFRAC                  = Parameter(index=[time, regions])            # Climate damages as share of gross output.
+    Y                        = Parameter(index=[time, regions])            # Gross world product net of abatement and damages (trillions 2005 USD yr⁻¹).
+    CPC                      = Parameter(index=[time, regions])            # Regional per capita consumption (thousands 2005 USD yr⁻¹)
+    industrial_emissions     = Parameter(index=[time, regions])            # Industrial carbon emissions (GtC yr⁻¹).
+    carbon_tax               = Parameter(index=[time, regions])            # Carbon tax ($/ton CO₂).
+    quintile_income_shares   = Parameter(index=[regions, quintiles])       # Quintile share of regional income.
+    damage_dist              = Parameter(index=[regions, quintiles])       # Quintile share of regional climate damages.
+    recycle_share            = Parameter(index=[regions, quintiles])       # Share of carbon tax revenue recycled back to each quintile.
+    qc_base                  = Parameter(index=[time, regions, quintiles]) # Pre-damage, pre-abatement cost, pre-tax quintile consumption (thousands 2005 USD yr⁻¹).
+
+    # --------------------
+    # Model Variables
+    # --------------------
+
+    pc_gdp                   = Variable(index=[time, regions])             # Per capita output net of abatement and damages (2005 USD / person).
+    CO₂_income_elasticity    = Variable(index=[time, regions])             # Elasticity of CO₂ price exposure with respect to income.
+    tax_revenue              = Variable(index=[time, regions])             # Total carbon tax revenue (2005 USD)
+    pc_tax_revenue           = Variable(index=[time, regions])             # Total per capita carbon tax revenue ($1000s/person)
+    abatement_cost_dist      = Variable(index=[time, regions, quintiles])  # Time-varying regional CO₂ tax distribution share by quintile.
+    carbon_tax_dist          = Variable(index=[time, regions, quintiles])  # Time-varying regional CO₂ abatement cost distribution share by quintile.
+    qc_post_damage_abatement = Variable(index=[time, regions, quintiles])  # Post-damage, post-abatement cost per capita quintile consumption (thousands 2005 USD/person yr⁻¹).
+    qc_post_tax              = Variable(index=[time, regions, quintiles])  # Quintile per capita consumption after subtracting out carbon tax (thousands 2005 USD/person yr⁻¹).
+    qc_post_recycle          = Variable(index=[time, regions, quintiles])  # Quintile per capita consumption after recycling tax back to quintiles (thousands 2005 USD/person yr⁻¹).
+
+
+    function run_timestep(p, v, d, t)
+
+        for r in d.regions
+
+            # Calculate net per capita income ($/person).
+            # Note, Y in $trillions and population in millions, so scale by 1e6.
+            v.pc_gdp[t,r] = p.Y[t,r] / p.regional_population[t,r] * 1e6
+
+            # Calculate time-varying income elasticity of CO₂ price exposure (requires pc_gdp units in $/person).
+            # Note, hold elasticity constant at boundary value if GDP falls outside the study support range.
+
+            if v.pc_gdp[t,r] < p.min_study_gdp
+                # GDP below observed study values.
+                v.CO₂_income_elasticity[t,r] = p.elasticity_intercept + p.elasticity_slope * log(p.min_study_gdp)
+            elseif v.pc_gdp[t,r] > p.max_study_gdp
+                # GDP above observed study values.
+                v.CO₂_income_elasticity[t,r] = p.elasticity_intercept + p.elasticity_slope * log(p.max_study_gdp)
+            else
+                # GDP within observed study values.
+                v.CO₂_income_elasticity[t,r] = p.elasticity_intercept + p.elasticity_slope * log(v.pc_gdp[t,r])
+            end
+
+            # Calculate total carbon tax revenue for each region (dollars).
+            # Note, emissions in GtC and tax in dollars, so scale by 1e9.
+            v.tax_revenue[t,r] = p.industrial_emissions[t,r] * p.carbon_tax[t,r] * 1e9
+
+            # Calculate per capita tax revenue for each region (convert to $1000s/person to match pc consumption units).
+            # Note, tax in dollars and population in millions, so scale by 1e12.
+            v.pc_tax_revenue[t,r] = v.tax_revenue[t,r] / p.regional_population[t,r] / 1e9
+
+            # Calculate quintile distribution share of CO₂ tax burden and mitigation costs (assume both distributions are equal).
+            v.abatement_cost_dist[t,r,:] = regional_quintile_distribution(v.CO₂_income_elasticity[t,r], p.quintile_income_shares[r,:])
+            v.carbon_tax_dist[t,r,:]     = regional_quintile_distribution(v.CO₂_income_elasticity[t,r], p.quintile_income_shares[r,:])
+
+            # Create a temporary variable used to calculate NICE baseline quintile consumption (just for convenience).
+            temp_C = 5.0 * p.CPC[t,r] * (1.0 + p.DAMFRAC[t,r]) / (1.0 - p.ABATEFRAC[t,r])
+
+            for q in d.quintiles
+
+                # Calculate post-damage, post-abatement cost per capita quintile consumption (bounded below to ensure consumptions don't collapse to zero or go negative).
+                # Note, this differs from standard NICE equation because quintile CO₂ abatement cost shares now vary over time.
+                v.qc_post_damage_abatement[t,r,q] = max(p.qc_base[t,r,q] - (5.0 * p.CPC[t,r] * p.DAMFRAC[t,r] * p.damage_dist[r,q]) - (temp_C * p.ABATEFRAC[t,r] * v.abatement_cost_dist[t,r,q]), 1e-8)
+
+                # Subtract tax revenue from each quintile based on quintile CO₂ tax burden distributions.
+                # Note, per capita tax revenue and consumption should both be in 1000s dollars/person.
+                v.qc_post_tax[t,r,q] = v.qc_post_damage_abatement[t,r,q] - (5 * v.pc_tax_revenue[t,r] * v.carbon_tax_dist[t,r,q])
+
+                # Recycle tax revenue by adding shares back to all quintiles (assume recycling shares constant over time).
+                v.qc_post_recycle[t,r,q] = v.qc_post_tax[t,r,q] + (5 * v.pc_tax_revenue[t,r] * p.recycle_share[r,q])
+            end
+        end
+    end
+end
